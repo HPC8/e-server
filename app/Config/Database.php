1672dc022ca0133cfe30d76fb40f155d4ccdf744
suthatburan <kung.suthatburan@gmail.com>
2020-09-30 16:19:42 +0700

    edit commit

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -33,8 +33,8 @@ class Database extends \CodeIgniter\Database\Config
 	public $default = [
 		'DSN'      => '',
 		'hostname' => '172.16.100.1',
-		'username' => 'administrator',
-		'password' => 'anamai#hpc8',
+		'username' => 'apps.hpc8',
+		'password' => 'pass.apps@hpc08',
 		'database' => 'e-service',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
